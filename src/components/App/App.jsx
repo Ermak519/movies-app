33560e8e7d071e46756c6e8a02585d42fe6cbabe
Vyacheslav <ermak519@gmail.com>
2022-01-25 16:45:34 +0300

    add: tabs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Offline, Online } from "react-detect-offline";
-import { Result } from 'antd';
+import { Result, Tabs, Empty } from 'antd';
+import { SearchOutlined, StarOutlined } from '@ant-design/icons';
 
 import { MovieList } from "../MovieList";
 import { SearchPanel } from "../SearchPanel";
@@ -29,18 +30,54 @@ export default class App extends Component {
 
     render() {
         const { request, currentPage } = this.state;
+        const { TabPane } = Tabs;
 
         return (
             <div className="app">
                 <Online>
-                    <SearchPanel
-                        request={request}
-                        onSearchMovie={this.onSearchMovie} />
-                    <MovieList
-                        request={request}
-                        currentPage={currentPage}
-                        onChangeCurrentPage={this.onChangeCurrentPage}
-                    />
+                    <Tabs defaultActiveKey="1" centered>
+                        <TabPane
+                        tab={
+                            <span>
+                                <SearchOutlined />
+                                Search
+                            </span>
+                        }
+                        key="1">
+                            <SearchPanel
+                                request={request}
+                                onSearchMovie={this.onSearchMovie} />
+                            <MovieList
+                                request={request}
+                                currentPage={currentPage}
+                                onChangeCurrentPage={this.onChangeCurrentPage}
+                            />
+                        </TabPane>
+                        <TabPane
+                        tab={
+                            <span>
+                                <StarOutlined />
+                                Rated
+                            </span>
+                        }
+                        key="2">
+                            <SearchPanel
+                                request={request}
+                                onSearchMovie={this.onSearchMovie} />
+                            <Empty
+                            image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+                            imageStyle={{
+                              height: 60,
+                            }}
+                            description={
+                              <span>
+                                Здесь пока ничего нет
+                              </span>
+                            }/>
+                        </TabPane>
+                        
+                    </Tabs>
+                    
                 </Online>
                 <Offline>
                     <Result
